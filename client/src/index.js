7fe396050003e5265ee4e331467d27d0b7213925
Bogdan Aksenov <bogdanaks@bk.ru>
2020-11-27 16:41:05 +0300

    Include store

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import './index.scss'
 import { App } from './App'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
 
-ReactDOM.render(
-    <React.StrictMode>
+const app = (
+    <Provider store={store}>
         <App />
-    </React.StrictMode>,
-    document.getElementById('root'),
+    </Provider>
 )
+
+render(app, document.getElementById('root'))
