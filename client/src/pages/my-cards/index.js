b46af1d74d57db91e0d66c233d3e7845ed8777e0
Bogdan Aksenov <bogdanaks@bk.ru>
2020-11-27 17:30:36 +0300

    Add dispatch to fetch data and set to components

--- client/src/pages/my-cards/index.js
+++ client/src/pages/my-cards/index.js
@@ -1,13 +1,33 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
-import styles from './styles.module.scss'
+import { fetchCards } from '../../redux/actions/cardAction'
 
 import { Header } from '../../components/ui/header'
+import { MiniCard } from '../../components/mini-card'
 
 export const MyCards = () => {
+    const dispatch = useDispatch()
+    const cards = useSelector((state) => state.card.cards)
+    const activeCard = useSelector((state) => state.card.activeCard)
+
+    React.useEffect(() => {
+        dispatch(fetchCards())
+        // eslint-disable-next-line
+    }, [])
+
     return (
         <>
             <Header title="Мои карты" backBtn={true} />
+            <ul>
+                {cards.map((item, index) => (
+                    <MiniCard
+                        card={item || []}
+                        key={index}
+                        activeCardNumb={activeCard.card_number}
+                    />
+                ))}
+            </ul>
         </>
     )
 }
