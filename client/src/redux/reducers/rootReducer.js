5ad70c32437b867a59fa19864f82db28008a0265
Bogdan Aksenov <bogdanaks@bk.ru>
2020-11-27 17:31:00 +0300

    Add card reducer

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 
 import { appReducer } from './appReducer'
+import { cardReducer } from './cardReducer'
 
 export const rootReducer = combineReducers({
     app: appReducer,
+    card: cardReducer,
 })
