ca62d425012ec100f1c0b42caa2cb2b4b0b45e9b
Bogdan Aksenov <bogdanaks@bk.ru>
2020-11-27 17:30:23 +0300

    Add dispatch to fetch data and set to components

--- client/src/pages/main/index.js
+++ client/src/pages/main/index.js
@@ -1,34 +1,39 @@
 import React from 'react'
-import api from '../../utils/api'
+import { useDispatch, useSelector } from 'react-redux'
 
-import styles from './styles.module.scss'
+import { fetchCards } from '../../redux/actions/cardAction'
 
-// import { MainLoader } from '../../components/loaders/main-loader'
+import { MainLoader } from '../../components/loaders/main-loader'
 import { Header } from '../../components/ui/header'
 import { Card } from '../../components/card'
 import { History } from '../../components/history'
 import { Currency } from '../../components/currency'
 
-export const Main = () => {
-    const [card, setCard] = React.useState({})
+import styles from './styles.module.scss'
 
+export const Main = () => {
+    const dispatch = useDispatch()
+    const activeCard = useSelector((state) => state.card.activeCard)
     React.useEffect(() => {
-        api.get(`api/card?&limit=1`).then((res) => {
-            setCard(res.data[0])
-        })
+        dispatch(fetchCards(1))
+        // eslint-disable-next-line
     }, [])
 
     return (
         <div className={styles.wrapper}>
             <Header title="Главная" />
-            <div className={styles.content}>
-                <Card card={card} />
-                <h2>Change currency</h2>
-                <Currency />
-                <div className={styles.history}>
-                    <History history={card.transaction_history || []} />
+            {!activeCard ? (
+                <MainLoader />
+            ) : (
+                <div className={styles.content}>
+                    <Card card={activeCard} />
+                    <h2>Change currency</h2>
+                    <Currency />
+                    <div className={styles.history}>
+                        <History card={activeCard} />
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
     )
 }
