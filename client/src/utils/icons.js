e96ca36f65d440a22932c35ec2b2e8b7712aa1f9
Bogdan Aksenov <bogdanaks@bk.ru>
2020-11-27 16:41:54 +0300

    Create icons importer

---
+++ client/src/utils/icons.js
@@ -0,0 +1,9 @@
+import mastercardIcon from '../assets/images/cards-icons/master-card.svg'
+import visaIcon from '../assets/images/cards-icons/visa.svg'
+import unionpayIcon from '../assets/images/cards-icons/unionpay.svg'
+
+export const icons = {
+    mastercard: mastercardIcon,
+    visa: visaIcon,
+    unionpay: unionpayIcon,
+}
