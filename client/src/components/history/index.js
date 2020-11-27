a2d96603103391bc83214e88819474c829472308
Bogdan Aksenov <bogdanaks@bk.ru>
2020-11-27 17:29:41 +0300

    Fix value

--- client/src/components/history/index.js
+++ client/src/components/history/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 import styles from './styles.module.scss'
 
-export const History = ({ history }) => {
+export const History = ({ card }) => {
     return (
         <div className={styles.wrapper}>
             <h3>History</h3>
-            {history.map((item, index) => (
+            {card.transaction_history.map((item, index) => (
                 <div className={styles.row} key={index}>
                     <div className={styles.imgBlock}>
                         <img src={item.icon_url} alt={item.title} />
