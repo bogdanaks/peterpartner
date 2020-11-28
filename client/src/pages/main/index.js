bdea574c3bce39965b44e8dcfdaa5578829dd50b
Bogdan Aksenov <bogdanaks@bk.ru>
2020-11-28 12:56:20 +0300

    Fix

--- client/src/pages/main/index.js
+++ client/src/pages/main/index.js
@@ -1,28 +1,29 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
-import { fetchCards } from '../../redux/actions/cardAction'
-
 import { MainLoader } from '../../components/loaders/main-loader'
 import { Header } from '../../components/ui/header'
 import { Card } from '../../components/card'
-import { History } from '../../components/history'
+import { History } from '../../components/history/index.js'
 import { Currency } from '../../components/currency'
+
+import { fetchCards } from '../../redux/actions/cardAction'
 
 import styles from './styles.module.scss'
 
 export const Main = () => {
     const dispatch = useDispatch()
     const activeCard = useSelector((state) => state.card.activeCard)
+
     React.useEffect(() => {
-        dispatch(fetchCards(1))
+        Object.keys(activeCard).length === 0 && dispatch(fetchCards(1))
         // eslint-disable-next-line
     }, [])
 
     return (
         <div className={styles.wrapper}>
             <Header title="Главная" />
-            {!activeCard ? (
+            {!Object.keys(activeCard).length ? (
                 <MainLoader />
             ) : (
                 <div className={styles.content}>
