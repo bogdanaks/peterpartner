05f8983b672f3e51d19f3bb379e697f01589d650
Bogdan Aksenov <bogdanaks@bk.ru>
2020-11-28 12:55:27 +0300

    Add type SET_CURRENCY

--- client/src/redux/actions/types.js
+++ client/src/redux/actions/types.js
@@ -1,5 +1,4 @@
-export const SHOW_ALERT = 'SHOW_ALERT'
-export const HIDE_ALERT = 'HIDE_ALERT'
+export const SET_CURRENCY = 'SET_CURRENCY'
 
 export const FETCH_CARDS = 'FETCH_CARDS'
 export const SET_CARD = 'SET_CARD'
