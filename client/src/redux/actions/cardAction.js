b617bc7ad543585aee97ce3f2ca8f1930d4f51a8
Bogdan Aksenov <bogdanaks@bk.ru>
2020-11-28 12:55:47 +0300

    Bug fix

--- client/src/redux/actions/cardAction.js
+++ client/src/redux/actions/cardAction.js
@@ -1,12 +1,18 @@
 import { FETCH_CARDS, SET_CARD } from './types'
 import api from '../../utils/api'
 
+import { store } from '../store'
+
 export const fetchCards = (limit) => {
     return async (dispatch) => {
         try {
+            const activeCard = store.getState().card.activeCard
             const res = await api.get(`api/card${limit !== undefined ? `?&limit=${limit}` : ''}`)
             dispatch({ type: FETCH_CARDS, payload: res.data })
-            dispatch({ type: SET_CARD, payload: res.data[0] })
+            dispatch({
+                type: SET_CARD,
+                payload: Object.keys(activeCard).length !== 0 ? activeCard : res.data[0],
+            })
         } catch (error) {
             console.error(error)
         }
@@ -14,7 +20,5 @@ export const fetchCards = (limit) => {
 }
 
 export const setActiveCard = (card) => {
-    return (dispatch) => {
-        dispatch({ type: SET_CARD, payload: card })
-    }
+    return { type: SET_CARD, payload: card }
 }
