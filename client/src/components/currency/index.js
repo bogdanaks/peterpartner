4b7a22ee0602997797a4cfd61849724f1a2c15e4
Bogdan Aksenov <bogdanaks@bk.ru>
2020-11-28 12:57:38 +0300

    Add handle for click currency

--- client/src/components/currency/index.js
+++ client/src/components/currency/index.js
@@ -1,20 +1,40 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { setActiveCurrency } from '../../redux/actions/appAction'
 
 import styles from './style.module.scss'
 
 export const Currency = () => {
+    const currentCurrency = useSelector((state) => state.app.currentCurrency)
+    const dispatch = useDispatch()
+
+    const handleClickCurrency = React.useCallback(
+        (symbol, name) => {
+            dispatch(setActiveCurrency(symbol, name))
+        },
+        // eslint-disable-next-line
+        [currentCurrency],
+    )
+
     return (
         <div className={styles.currency}>
             <ul>
-                <li className={styles.active}>
+                <li
+                    className={currentCurrency.name === 'GBP' ? styles.active : ''}
+                    onClick={() => handleClickCurrency('£', 'GBP')}>
                     <span className={styles.ico}>£</span>
                     <span className={styles.title}>GBP</span>
                 </li>
-                <li>
+                <li
+                    className={currentCurrency.name === 'EUR' ? styles.active : ''}
+                    onClick={() => handleClickCurrency('€', 'EUR')}>
                     <span className={styles.ico}>€</span>
                     <span className={styles.title}>EUR</span>
                 </li>
-                <li>
+                <li
+                    className={currentCurrency.name === 'RUB' ? styles.active : ''}
+                    onClick={() => handleClickCurrency('₽', 'RUB')}>
                     <span className={styles.ico}>₽</span>
                     <span className={styles.title}>RUB</span>
                 </li>
