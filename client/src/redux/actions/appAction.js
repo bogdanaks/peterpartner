4610dc77bb260600b2c58e51d27b8a5145a97198
Bogdan Aksenov <bogdanaks@bk.ru>
2020-11-28 16:54:02 +0300

    Add types for show and hide error

--- client/src/redux/actions/appAction.js
+++ client/src/redux/actions/appAction.js
@@ -1,4 +1,12 @@
-import { SET_CURRENCY } from './types'
+import { SET_CURRENCY, SHOW_ERROR, HIDE_ERROR } from './types'
+
+export const showError = (message) => {
+    return { type: SHOW_ERROR, payload: message }
+}
+
+export const hideError = () => {
+    return { type: HIDE_ERROR }
+}
 
 export const setActiveCurrency = (symbol, name) => {
     return { type: SET_CURRENCY, payload: { symbol, name } }
