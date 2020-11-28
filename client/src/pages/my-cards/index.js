49d4c545ecda39f2609111be1d42aaa3dac28d7a
Bogdan Aksenov <bogdanaks@bk.ru>
2020-11-28 13:22:22 +0300

    Check if data not loaded, set mini loader

--- client/src/pages/my-cards/index.js
+++ client/src/pages/my-cards/index.js
@@ -5,6 +5,7 @@ import { fetchCards } from '../../redux/actions/cardAction'
 
 import { Header } from '../../components/ui/header'
 import { MiniCard } from '../../components/mini-card'
+import { MiniLoader } from '../../components/loaders/mini-loader'
 
 export const MyCards = () => {
     const dispatch = useDispatch()
@@ -16,18 +17,26 @@ export const MyCards = () => {
         // eslint-disable-next-line
     }, [])
 
+    if (!cards) {
+        return <MiniLoader />
+    }
+
     return (
         <>
             <Header title="Мои карты" backBtn={true} />
-            <ul>
-                {cards.map((item, index) => (
-                    <MiniCard
-                        card={item || []}
-                        key={index}
-                        activeCardNumb={activeCard.card_number}
-                    />
-                ))}
-            </ul>
+            {cards.length === 0 ? (
+                <MiniLoader />
+            ) : (
+                <ul>
+                    {cards.map((item, index) => (
+                        <MiniCard
+                            card={item || []}
+                            key={index}
+                            activeCardNumb={activeCard.card_number}
+                        />
+                    ))}
+                </ul>
+            )}
         </>
     )
 }
