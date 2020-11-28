ccd9884661c401994a24936a2073b46270b576a0
Bogdan Aksenov <bogdanaks@bk.ru>
2020-11-28 16:51:57 +0300

    Add action for show and hide error

--- client/src/redux/actions/types.js
+++ client/src/redux/actions/types.js
@@ -1,4 +1,6 @@
 export const SET_CURRENCY = 'SET_CURRENCY'
+export const SHOW_ERROR = 'SHOW_ERROR'
+export const HIDE_ERROR = 'HIDE_ERROR'
 
 export const FETCH_CARDS = 'FETCH_CARDS'
 export const SET_CARD = 'SET_CARD'
