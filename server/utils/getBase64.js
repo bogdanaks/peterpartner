efd340ad8707675e3277e328fbc8f8824bc6d13a
Bogdan Aksenov <bogdanaks@bk.ru>
2020-11-28 12:54:42 +0300

    Fix

--- server/utils/getBase64.js
+++ server/utils/getBase64.js
@@ -1,17 +1,21 @@
 const request = require('request-promise')
 
-const getBase64 = async (url) => {
-    const result = await request.get(url, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            data =
-                'data:' +
-                response.headers['content-type'] +
-                ';base64,' +
-                Buffer.from(body).toString('base64')
-            return data
-        }
-    })
-    return result
+const getBase64 = async (url, res) => {
+    try {
+        const result = await request.get(url, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                data =
+                    'data:' +
+                    response.headers['content-type'] +
+                    ';base64,' +
+                    Buffer.from(body).toString('base64')
+                return data
+            }
+        })
+        return result
+    } catch (error) {
+        return res.status(500).send({ message: 'Server error' })
+    }
 }
 
 module.exports = getBase64
