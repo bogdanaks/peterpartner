0d6440955c2417ec774d7283d8ca4a99cf9362ff
Bogdan Aksenov <bogdanaks@bk.ru>
2020-11-28 12:58:06 +0300

    Add handle for click card

--- client/src/components/mini-card/index.js
+++ client/src/components/mini-card/index.js
@@ -1,13 +1,29 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+
+import { setActiveCard } from '../../redux/actions/cardAction'
 
 import { icons } from '../../utils/icons'
+
 import styles from './styles.module.scss'
 
-export const MiniCard = ({ card }) => {
+export const MiniCard = ({ card, activeCardNumb }) => {
+    const dispatch = useDispatch()
+    const history = useHistory()
+
+    const handleCardClick = () => {
+        dispatch(setActiveCard(card))
+        history.push('/')
+    }
+
     return (
-        <li className={styles.wrapper}>
+        <li className={styles.wrapper} onClick={handleCardClick}>
             <img src={icons[card.type]} alt={card.type} />
             <span>{card.card_number}</span>
+            {card.card_number === activeCardNumb ? (
+                <div className={styles.activeCircle}></div>
+            ) : null}
         </li>
     )
 }
